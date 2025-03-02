348d790c061a0ac7de56ecf40cc07750a377c411
muhammetsarican <muhammetsarican@gmail.com>
2025-03-02 23:03:53 +0300

    refactor: authenticate access token middleware added to base route's all route.

--- server/src/routes/BaseRoute.js
+++ server/src/routes/BaseRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const validate = require("../middlewares/validate");
+const { AuthenticateAccessToken } = require("../middlewares/authenticate");
 
 class BaseRoute {
     constructor(Controller, Schemas, Router = express.Router()) {
@@ -8,19 +9,19 @@ class BaseRoute {
         this.Router = Router;
     }
     listOne() {
-        this.Router.route("/:id").get(this.Controller.listOne());
+        this.Router.route("/:id").get(AuthenticateAccessToken, this.Controller.listOne());
     }
     list() {
-        this.Router.route("/").get(this.Controller.index());
+        this.Router.route("/").get(AuthenticateAccessToken, this.Controller.index());
     }
     create() {
-        this.Router.route("/").post(validate(this.Schemas.createValidation), this.Controller.create());
+        this.Router.route("/").post(AuthenticateAccessToken, validate(this.Schemas.createValidation), this.Controller.create());
     }
     update() {
-        this.Router.route("/:id").patch(validate(this.Schemas.updateValidation), this.Controller.update());
+        this.Router.route("/:id").patch(AuthenticateAccessToken, validate(this.Schemas.updateValidation), this.Controller.update());
     }
     delete() {
-        this.Router.route("/:id").delete(this.Controller.delete());
+        this.Router.route("/:id").delete(AuthenticateAccessToken, this.Controller.delete());
     }
     IndexRoutes() {
         this.listOne();
