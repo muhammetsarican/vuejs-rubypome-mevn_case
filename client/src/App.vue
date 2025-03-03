df559ada67648640ba908588ed10f126bdfb4339
muhammetsarican <muhammetsarican@gmail.com>
2025-03-03 23:23:12 +0300

    refactor: check token query added to app component and conditions added for show menus for user account.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,34 @@
 <template>
   <div>
-    <router-link to="/appointments">Appointments</router-link>
-    <router-link to="/login">Login</router-link>
-    <router-link to="/register">Register</router-link>
+    <div v-if="_getCurrentUser">
+      <router-link to="/appointments">Appointments</router-link>
+      <p v-if="_getCurrentUser.fullname">{{ _getCurrentUser.fullname }}</p>
+      <p v-else>{{ _getCurrentUser.mail }}</p>
+    </div>
+    <div v-else>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/register">Register</router-link>
+    </div>
     <router-view></router-view>
   </div>
 </template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["_getCurrentUser"]),
+  },
+  mounted() {
+    this.$appAxios
+      .request("/user/check-token", {
+        method: "get",
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        this.$store.commit("saveUser", data.message.user);
+        this.$$appAxios.setHeader(data.message.tokens.access_token);
+      })
+      .catch((err) => console.log(err.message));
+  },
+};
+</script>
