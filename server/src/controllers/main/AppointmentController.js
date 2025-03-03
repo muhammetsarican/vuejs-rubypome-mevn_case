f9bdebc994b5f2e6d6444558fb96c3836660d74e
muhammetsarican <muhammetsarican@gmail.com>
2025-03-04 00:12:04 +0300

    refactor: list and create routes overrided for use user id field when creating and listing datas.

--- server/src/controllers/main/AppointmentController.js
+++ server/src/controllers/main/AppointmentController.js
@@ -21,6 +21,41 @@ class AppointmentController extends BaseController {
                 })
         }
     }
+
+    create() {
+        return (req, res, next) => {
+            const data = {
+                user_id: req.params.user_id,
+                date: req.body.date,
+                time: req.body.time,
+            }
+
+            this.Service.insert(data)
+                .then(response => {
+                    res.status(201).send({
+                        success: true,
+                        message: response
+                    })
+                })
+        }
+    }
+
+    index() {
+        return (req, res, next) => {
+            const where = {
+                user_id: req.params.user_id
+            }
+
+            this.Service.list(where)
+                .then(response => {
+                    if (!response.length) return next(new Error("No records found!"));
+                    res.status(200).send({
+                        success: true,
+                        message: response
+                    })
+                })
+        }
+    }
 }
 
 module.exports = new AppointmentController();
--- server/src/routes/main/AppointmentRoute.js
+++ server/src/routes/main/AppointmentRoute.js
@@ -5,6 +5,7 @@ const BaseRoute = require("../BaseRoute");
 const AppointmentController = require("../../controllers/main/AppointmentController");
 const schemas = require("../../validations/main/AppointmentValidation");
 const { AuthenticateAccessToken } = require("../../middlewares/authenticate");
+const validate = require("../../middlewares/validate");
 
 const AppointmentRouter = express.Router();
 
@@ -14,11 +15,23 @@ class AppointmentRoute extends BaseRoute {
     }
 
     slotAppointments() {
-        this.Router.route("/slot-appointments").get(this.Controller.slotAppointments());
+        this.Router.route("/slot-appointments").get(AuthenticateAccessToken, this.Controller.slotAppointments());
+    }
+
+    // ? this methods overrided for use user id when doing this operation
+    create() {
+        this.Router.route("/:user_id").post(AuthenticateAccessToken, validate(schemas.createValidation), this.Controller.create());
+    }
+
+    // ? this methods overrided for use user id when doing this operation
+    list() {
+        this.Router.route("/:user_id").get(AuthenticateAccessToken, this.Controller.index());
     }
 
     IndexRoutes() {
         this.slotAppointments();
+        this.create();
+        this.list();
         return super.IndexRoutes();
     }
 }
