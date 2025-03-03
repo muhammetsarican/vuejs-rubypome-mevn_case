2d84a9d3be3ce6a2a83831f4773eeb6d2fe8bca4
muhammetsarican <muhammetsarican@gmail.com>
2025-03-03 23:21:10 +0300

    feat: axios added for fetching api and added to global properties.

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,12 @@ import router from './router';
 import store from './store';
 
 import axios from "axios";
+import { appAxios } from './utils/appAxios';
 
 const app = createApp(App);
 
 app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$appAxios = appAxios;
 
 app.use(router);
 app.use(store);
---
+++ client/src/utils/appAxios.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+class AppAxios {
+    constructor() {
+        this.request = axios.create({
+            baseURL: "http://localhost:4040/api",
+            withCredentials: true,
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Headers': 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent, X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range',
+                'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,PUT,DELETE,PATCH',
+            }
+        });
+    }
+
+    setHeader(token) {
+        this.request.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    }
+};
+
+export const appAxios = new AppAxios();
