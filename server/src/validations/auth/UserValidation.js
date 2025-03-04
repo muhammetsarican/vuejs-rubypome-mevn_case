db2f2155edd2029edda2c9b4f2a0b83ea3480e29
muhammetsarican <muhammetsarican@gmail.com>
2025-03-04 21:26:23 +0300

    refactor: password max character count increased.

--- server/src/validations/auth/UserValidation.js
+++ server/src/validations/auth/UserValidation.js
@@ -3,13 +3,13 @@ const joi = require("joi");
 const createValidation = joi.object({
     fullname: joi.string().min(3).max(30),
     mail: joi.string().min(3).max(50).required(),
-    password: joi.string().min(8).max(20).required()
+    password: joi.string().min(8).max(70).required()
 })
 
 const updateValidation = joi.object({
     fullname: joi.string().min(3).max(30),
     mail: joi.string().min(3).max(50),
-    password: joi.string().min(8).max(20)
+    password: joi.string().min(8).max(70)
 })
 
 module.exports = {
